ede8edc4328e1b2caeee90331ddb83e82a0675aa
ToritseTuedor <torisevalerie@gmail.com>
2023-07-11 17:39:19 -0400

    made changes to the design, fixed side bar changed, layour of content chnaged aswell, pages also chnaged to reflect changes and started responsiveness changes

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@
 // On scroll, change nav to fixed but make it stay within the contsiner of scrollable content and adjust when the width decreases
 // when the scroll is at the top of the page remove the fixed element and make it normal again
 
-//Home should have an underline when it is on the home page and should switch when it goes on different pages 
+
 
 const scrollableSection = document.querySelector(".changing-content");
 const handleScroll = () => {
@@ -20,3 +20,13 @@ const handleScroll = () => {
   }
 };
 scrollableSection.addEventListener("scroll", handleScroll);
+
+//NEXT STEPS
+
+//Home should have an underline when it is on the home page and should switch when it goes on different pages 
+
+// Make scroll transition smoother 
+
+// Project Page 
+// When user clicks the eye icon show the full project & take away the tags beside the intro 
+// Change starting eye icon to closed eye, When user clicks on icon make eye icon open 
