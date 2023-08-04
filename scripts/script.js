fa1ef27af6137be2249379af6c5a7b0c54e3843e
ToritseTuedor <torisevalerie@gmail.com>
2023-08-03 22:10:58 -0400

    all pages done, minor chnages to html and css being made

--- scripts/script.js
+++ scripts/script.js
@@ -1,71 +1,29 @@
-// Navigation Bar Fixed When Scroll Occurs
+// Create a variable to hold the navigation bar
+// Add an event listener for the navigation-bar
+// Create a function that on click displays
 
-// Add event listener to listen for scroll of .changing-content
-// On scroll, change nav to fixed but make it stay within the contsiner of scrollable content and adjust when the width decreases
-// when the scroll is at the top of the page remove the fixed element and make it normal again
+const navigationMenu = document.querySelector(".navigationBar");
+const hiddenNav = document.querySelector(".hiddenNav");
+console.log(hiddenNav);
+const closeNav = document.querySelector(".closingButton");
+const navIcons = document.querySelector(".navIcons");
+const hiddenNavContent = document.querySelector(".hiddenNavContent")
 
-const scrollableSection = document.querySelector(".changing-content");
-const handleScroll = () => {
-  const nav = document.querySelector(".main-navigation-bar");
-  console.log(nav);
-  const scrollPosition = scrollableSection.scrollTop;
-    console.log(scrollPosition);
-  if (scrollPosition > 0) {
-    nav.style.position = "fixed";
-    nav.style.width = "58%";
-  } else if (scrollPosition === 0) {
-    nav.style.position = "";
-    nav.style.width = "";
-  }
+const openNav = () => {
+  hiddenNav.style.width = "420px";
+  setTimeout(() => {
+    navigationMenu.style.zIndex = "1";
+    hiddenNav.style.zIndex = "2";
+    hiddenNavContent.style.display = "block";
+    
+  }, 400);
 };
-scrollableSection.addEventListener("scroll", handleScroll);
+const closeHiddenNav = () => {
+  hiddenNav.style.width = "0";
+  navigationMenu.style.zIndex = "2";
+  hiddenNav.style.zIndex = "1";
+  hiddenNavContent.style.display = "none";
+};
 
-//NEXT STEPS
-
-//Home should have an underline when it is on the home page and should switch when it goes on different pages
-
-// Make scroll transition smoother
-
-// Project Page
-// When user clicks the eye icon show the full project & take away the tags beside the intro
-// Change starting eye icon to closed eye, When user clicks on icon make eye icon open
-
-// query select the view button 
-//query select the tag ul 
-// add an event listener to listen for a click
-// create a function called handleEyeButtonClick 
-  // create a variable to hold the eye button 
-  // create a variable that will hold the tags ul
-  // eyeButton.style.display ="block"
-  //tags.style.display="none"
-// when user clicks on button again close the dropdown menu and bring back the tags
-
-// FireBase
-
-import app from "./firebaseConfig.js";
-import {
-  getDatabase,
-  ref,
-  get,
-} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js"; // "app" was changed to the word database
-
-// reference to the database
-const database = getDatabase(app);
-
-//reference to the recentProjects & projects section of the database
-const recentProjectRef = ref(database, "/recentProjects");
-const projects = ref(database, "/projects");
-
-// Getting our recentProjects data from our database and displaying it on the page
-
-get(recentProjectRef).then((snapshot) => {
-  const recentProjectData = snapshot.val();
-  // loop through the recentproject ref array and display it on the page
-
-  // NEXT STEPS: Explain For Loop 
-
-    // Use ForLoop - why?
-    // Set a variable for each project object - why?
-    // Append HTML for recentProjects to the Page
-
-});
+navigationMenu.addEventListener("click", openNav);
+closeNav.addEventListener("click", closeHiddenNav);
